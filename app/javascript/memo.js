5c216a6a646a5ddab3c0db5b550535db76e73a3f
yoshiocamp <yoshio14393974@gmail.com>
2024-05-21 16:05:11 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,6 +2,7 @@ function post (){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+  // form.addEventListener("submit", () => {
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
